3875bd32471b82b06398bc77405f24a61caa7419
Kent Fredric <kentnl@cpan.org>
2013-10-23 12:39:43 +0000

    Import of KENTNL/Gentoo-Dependency-AST-0.001001 from CPAN.
    gitpan-cpan-distribution: Gentoo-Dependency-AST
    gitpan-cpan-version:      0.001001
    gitpan-cpan-path:         KENTNL/Gentoo-Dependency-AST-0.001001.tar.gz
    gitpan-cpan-author:       KENTNL
    gitpan-cpan-maturity:     released

--- lib/Gentoo/Dependency/AST/Node/Group/And.pm
+++ lib/Gentoo/Dependency/AST/Node/Group/And.pm
@@ -6,10 +6,11 @@ BEGIN {
   $Gentoo::Dependency::AST::Node::Group::And::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Gentoo::Dependency::AST::Node::Group::And::VERSION = '0.001000';
+  $Gentoo::Dependency::AST::Node::Group::And::VERSION = '0.001001';
 }
 
 # ABSTRACT: A Group of dependencies of which, all are required
+
 
 use parent 'Gentoo::Dependency::AST::Node';
 
@@ -19,7 +20,7 @@ __END__
 
 =pod
 
-=encoding utf-8
+=encoding UTF-8
 
 =head1 NAME
 
@@ -27,7 +28,18 @@ Gentoo::Dependency::AST::Node::Group::And - A Group of dependencies of which, al
 
 =head1 VERSION
 
-version 0.001000
+version 0.001001
+
+=begin MetaPOD::JSON v1.1.0
+
+{
+    "namespace":"Gentoo::Dependency::AST::Node::Group::And",
+    "interface":"class",
+    "inherits":"Gentoo::Dependency::AST::Node"
+}
+
+
+=end MetaPOD::JSON
 
 =head1 AUTHOR
 
--- lib/Gentoo/Dependency/AST/Node/Group/NotUse.pm
+++ lib/Gentoo/Dependency/AST/Node/Group/NotUse.pm
@@ -7,10 +7,11 @@ BEGIN {
   $Gentoo::Dependency::AST::Node::Group::NotUse::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Gentoo::Dependency::AST::Node::Group::NotUse::VERSION = '0.001000';
+  $Gentoo::Dependency::AST::Node::Group::NotUse::VERSION = '0.001001';
 }
 
 # ABSTRACT: A group of dependencies that require a C<useflag> disabled to trigger require
+
 
 use parent 'Gentoo::Dependency::AST::Node::Group::Use';
 
@@ -20,7 +21,7 @@ __END__
 
 =pod
 
-=encoding utf-8
+=encoding UTF-8
 
 =head1 NAME
 
@@ -28,7 +29,18 @@ Gentoo::Dependency::AST::Node::Group::NotUse - A group of dependencies that requ
 
 =head1 VERSION
 
-version 0.001000
+version 0.001001
+
+=begin MetaPOD::JSON v1.1.0
+
+{
+    "namespace":"Gentoo::Dependency::AST::Node::Group::NotUse",
+    "interface":"class",
+    "inherits":"Gentoo::Dependency::AST::Node::Group::Use"
+}
+
+
+=end MetaPOD::JSON
 
 =head1 AUTHOR
 
